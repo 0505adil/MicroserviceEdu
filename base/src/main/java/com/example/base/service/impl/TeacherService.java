7a473f65963d7590cb2ecfabede19ad79e85dcc8
0505adil <0505adil00@gmail.com>
2021-11-29 23:04:31 +0600

    Apache Kafka Realization

--- base/src/main/java/com/example/base/service/impl/TeacherService.java
+++ base/src/main/java/com/example/base/service/impl/TeacherService.java
@@ -1,11 +1,13 @@
 package com.example.base.service.impl;
 
+import com.example.base.config.KafkaSender;
 import com.example.base.model.Document;
 import com.example.base.model.Order;
 import com.example.base.model.Teacher;
 import com.example.base.repository.TeacherRepo;
 import com.example.base.service.iTeacherService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class TeacherService implements iTeacherService {
 
     @Autowired
@@ -21,9 +24,12 @@ public class TeacherService implements iTeacherService {
     @Autowired
     private RestTemplate restTemplate;
 
+    private KafkaSender kafkaSender;
+
     @Override
     @HystrixCommand(fallbackMethod = "createFallback")
     public Object  createTeacher(Teacher teacher) {
+        kafkaSender.sendMessage("Attemp to create Teacher", "topic-1");
         List<Document> doc = restTemplate.getForObject("http://localhost:8084/api/clients/employees", List.class);
         Order order = new Order(null, "Teacher Salary", "150000");
         String resFinance = restTemplate.postForObject("http://localhost:8083/finance/create", order, String.class  );
--- zuul/src/main/java/com/example/zuul/security/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.example.zuul.security;
-
-public enum UserRole {
-	USER, ADMIN;
-}
